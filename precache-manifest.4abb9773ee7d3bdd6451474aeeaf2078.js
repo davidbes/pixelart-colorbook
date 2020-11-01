d4054e04e9849584dd842276b6eaa6998d4755ed
David Bester <david.projects.01@gmail.com>
2020-11-01 16:35:07 +0100

    Updates

---
+++ precache-manifest.4abb9773ee7d3bdd6451474aeeaf2078.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe4b205f63f9bdda813c2e8314ec43ce",
+    "url": "/pixelart-colorbook/index.html"
+  },
+  {
+    "revision": "e8a63c9d54b055a5b445",
+    "url": "/pixelart-colorbook/static/css/main.1aba2a3d.chunk.css"
+  },
+  {
+    "revision": "0168b3962d33d5d8068b",
+    "url": "/pixelart-colorbook/static/js/2.7374ed73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pixelart-colorbook/static/js/2.7374ed73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8a63c9d54b055a5b445",
+    "url": "/pixelart-colorbook/static/js/main.f1b82e5e.chunk.js"
+  },
+  {
+    "revision": "1b1367d0f053e043f888",
+    "url": "/pixelart-colorbook/static/js/runtime-main.c91c34a3.js"
+  },
+  {
+    "revision": "91cab6adef5ca13fbf06da502e4c6ffc",
+    "url": "/pixelart-colorbook/static/media/grid.91cab6ad.png"
+  },
+  {
+    "revision": "aaf7c4196e8e5214c3d67e9f6dd4412e",
+    "url": "/pixelart-colorbook/static/media/upload-image.aaf7c419.svg"
+  }
+]);
